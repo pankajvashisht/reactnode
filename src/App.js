d7cfc50012d7b68ec2b5a2e22149d53d1992510c
Pankaj Vashisht <pankaj@ucreate.co.in>
2019-08-25 11:20:29 +0530

    layout changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { BrowserRouter as Router, Route, Redirect } from "react-router-dom";
 
 import routes from "./routes";
 import withTracker from "./withTracker";
@@ -7,26 +7,35 @@ import withTracker from "./withTracker";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./asset/styles/shards-dashboards.1.1.0.min.css";
 
-export default () => (
-  <Router basename={process.env.REACT_APP_BASENAME || ""}>
-    <div>
-      {routes.map((route, index) => {
-        return (
-          <Route
-            key={index}
-            path={route.path}
-            exact={route.exact}
-            component={withTracker(props => {
-              return (
-                <route.layout {...props}>
-                  <route.component {...props} />
-                </route.layout>
-              );
-            })}
-          />
-        );
-      })}
-    </div>
-  </Router>
-);
 
+
+export default () => {
+  const [title,updateTitle] = useState('Home'); 
+  const login_datails = localStorage.getItem('userInfo');
+  useEffect(() => {
+    document.title = `2TigersLCC-${title}`;
+  })
+   return(
+          <Router basename={process.env.REACT_APP_BASENAME || ""}>
+            <div>
+              {routes.map((route, index) => {
+                return (
+                  <Route
+                    key={index}
+                    path={route.path}
+                    exact={route.exact}
+                    component={withTracker(props => {
+                      updateTitle(route.page)
+                      return (
+                        <route.layout {...props}>
+                          <route.component {...props} />
+                        </route.layout>
+                      );
+                    })}
+                  />
+                );
+              })}
+            </div>
+          </Router>
+        );
+    }
