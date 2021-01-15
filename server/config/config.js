20233a358b149ed7a651946a30ccdd3fb01ea38e
pankajvashisht-ucreate <pankaj@ucreate.co.in>
2021-01-15 22:06:18 +0530

    change name

--- server/config/config.js
+++ server/config/config.js
@@ -4,7 +4,7 @@ const path = require('path');
 const config = {
 	port: process.env.port || 4000,
 	root_path: path.resolve(__dirname).replace('config', 'public'),
-	AppName: 'Radio',
+	AppName: 'Readio',
 	GOOGLE_KEY:
 		'AAAAcGgF5QQ:APA91bEBD28ByjY_x53Ut1V2ZXvfBai_t2s6pJEoPEjUEYfwtG-IpM0Al5J65lU-Ifvmol0-5nCsRWHvCHYmnPZ9iJRbYqOdzK1-wgXjn1U2TdmexOuB6VMK7pt0TIpJhPgjwbjJtIFl',
 };
